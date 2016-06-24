10847e34c9cf01340c752642b6e41f484d3cedfc
MilesChou <jangconan@gmail.com>
2016-06-24 15:44:46 +0800

    Initial tasks resource

--- public/index.php
+++ public/index.php
@@ -4,8 +4,31 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
 
-$app = new \Slim\App;
+$app = new \Slim\App();
+
+$app->get('/tasks', function (Request $request, Response $response, $args) {
+    return $response;
+});
+
+$app->get('/tasks/{id}', function (Request $request, Response $response, $args) {
+    return $response;
+});
+
+$app->post('/tasks', function (Request $request, Response $response, $args) {
+    return $response;
+});
+
+$app->put('/tasks/{id}', function (Request $request, Response $response, $args) {
+    return $response;
+});
+
+$app->delete('/tasks/{id}', function (Request $request, Response $response, $args) {
+    return $response;
+});
+
 $app->get('/', function (Request $request, Response $response) {
+    $setting = $this->get('settings');
+    var_dump($setting);
     $response->getBody()->write("Hello World!");
     return $response;
 });
