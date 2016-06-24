fdf1397d9617f2e72dc81863c24f7355c7601503
MilesChou <jangconan@gmail.com>
2016-06-24 16:54:15 +0800

    Add Task service

---
+++ src/Service/Task.php
@@ -0,0 +1,46 @@
+<?php
+namespace Todos\Service;
+
+use RedBeanPHP\Facade as R;
+
+class Task
+{
+    public function getList()
+    {
+        $tasks = R::findAll('tasks');
+
+        return $tasks;
+    }
+
+    public function create($data)
+    {
+        $task = R::dispense('tasks');
+        $task->title = $data['title'];
+        $task->description = $data['description'];
+
+        $id = R::store($task);
+    }
+
+    public function find(int $id)
+    {
+        $task = R::find('tasks', $id);
+
+        return $task;
+    }
+
+    public function update(int $id, array $data)
+    {
+        $task = $this->find($id);
+        $task->title = $data['title'];
+        $task->description = $data['description'];
+
+        $id = R::store($task);
+    }
+
+    public function remove(int $id)
+    {
+        $task = $this->find($id);
+
+        R::trash($task);
+    }
+}
